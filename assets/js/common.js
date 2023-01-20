c84e3afd8822d52729788a60dae62b7885bf1622
fenix <fenix.azul09@protonmail.com>
2023-01-19 22:06:23 -0300

    cor da pagina

--- assets/js/common.js
+++ assets/js/common.js
@@ -98,3 +98,24 @@ const styleSwitcherToggle = document.querySelector('.style__switcher-toggler'),
 styleSwitcherToggle.addEventListener('click', () => {
   styleSwitcher.classList.toggle('open');
 })
+
+// ocultar switcher com o rolar da página
+window.addEventListener('scroll', () => {
+  if(styleSwitcher.classList.contains('open')) {
+    styleSwitcher.classList.remove('open');
+  }
+})
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color) {
+
+  alternateStyles.forEach((style) => {
+
+    if(color === style.getAttribute('title')) {    
+      style.removeAttribute('disabled');
+    } else {
+      style.setAttribute('disabled', 'true');
+    }
+  })
+}
